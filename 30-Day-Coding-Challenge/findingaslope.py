a0cdd920d35e9e070081d7d42f4b4c7e08a75a67
Tammy Barbee <tgmiller5@users.noreply.github.com>
2016-07-19 02:55:07 -0400

    Create findingaslope.py

---
+++ 30-Day-Coding-Challenge/findingaslope.py
@@ -0,0 +1,18 @@
+#This is a program on finding the slope:
+
+print("This is a program to find the slope of a line that passes through points")
+print("Enter points for x1,x2, y1, and y2 to find the slope")
+      
+ 
+y1 = float(raw_input("Enter y1: "))
+y2 = float(input("Enter y2: "))
+rise = y2 -y1
+
+ 
+x1 = float(raw_input("Enter x1:"))
+x2 = float(raw_input("Enter x2: "))
+run = x2 - x1
+
+ 
+m = rise/run
+print ("The slope will be: ", m)
