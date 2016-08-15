12292ba1d5f867d96c594dfab39d447a3b3aaf9e
Tammy Barbee <tgmiller5@users.noreply.github.com>
2016-08-15 12:09:16 -0400

    Update presentvalue.py

--- Data-Science/Finance/presentvalue.py
+++ Data-Science/Finance/presentvalue.py
@@ -1,1 +1,15 @@
 
+#formula for growing perpetituity is C/R-g
+#Cash flow number is 12.50
+#discount rate which is R will be 8.5
+#constant growth rate which is g will be 2.5
+
+
+#Create input values
+cash_flow = float(raw_input("Enter a cash flow number: "))
+discount_rate = float(raw_input("Enter a discount rate: "))
+growth_rate = float(raw_input("Enter a growth rate: "))
+pvgrow = cash_flow/(discount_rate - growth_rate)
+
+#print out value and round to two decimal places
+print "The present value of this growing perpetuity is:", round(pvgrow, 2)
